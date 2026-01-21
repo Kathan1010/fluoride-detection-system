071e43d0811526725c39c2d2f3223e0bdb46afea
Kathan1010 <kathangajera@gmail.com>
2026-01-21 11:49:33 +0530

    deployment changes

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,7 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import numpy as np
